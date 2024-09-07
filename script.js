6d2d8600177a9d8c00724ac80c98a91fbfd319cf
shubham4489 <143184073+shubham4489@users.noreply.github.com>
2024-09-07 13:16:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+function mapLetters(word){
+	let letterMap={};
+	for(let i=0;i<word.length;i++){
+		let char=word[i];
+		if(letterMap[char]){
+			letterMap[char].push(i);
+		}
+			else{
+			letterMap[char]=[i];
+			}
+		}
+	return letterMap;
+}
